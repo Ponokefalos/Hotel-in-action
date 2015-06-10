4fc188bd7dcec8edb418fb93b6e8ac92e8b53e08
GlitchAintOne <kotzias.ar@gmail.com>
2015-06-10 20:43:34 +0300

    emfanizi ta 3enodoxia hotel.php

--- scr/ArizFunctions.php
+++ scr/ArizFunctions.php
@@ -5,3 +5,24 @@
  * Date: 10/6/2015
  * Time: 7:44 μμ
  */
+function get_hotels()
+{
+    global $link;
+    require('RegisterConnectToDB.php');
+
+    $sql = "SELECT * FROM hotels";
+    $result = $link ->query($sql);
+
+    if ($result->num_rows > 0) {
+        $link->close();
+        return ($result);
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+}
+
