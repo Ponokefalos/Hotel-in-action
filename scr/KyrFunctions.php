e08a759ce6f34be7ed8d1fdaeec45984c877cdca
Ponokefalos <pesrekaisi@gmail.com>
2015-06-11 22:06:45 +0300

    wannabe change

--- scr/KyrFunctions.php
+++ scr/KyrFunctions.php
@@ -5,3 +5,44 @@
  * Date: 10/6/2015
  * Time: 7:44 μμ
  */
+
+
+function saveNewHotelOnDatabase($hotelName, $shortDesc, $longDesc, $date, $link, $userID,$image,$kouzinaBo,$theaBox,$tvBox,$wifiBox,$wcBox,$parkingBox,$acBox,$poolBox)
+{
+    mysqli_autocommit($link, false);
+
+    $query = "insert into hotels
+                             (
+                                 HotelName,
+                                 ShortDesc,
+                                 Description,
+                                 user_id,
+                                 Date,
+                                 image
+                             )
+                             Values
+                             (
+                                '$hotelName',
+                                '$shortDesc',
+                                '$longDesc',
+                                '$userID',
+                                '$date',
+                                '$image'
+                             )";
+
+    $result = mysqli_query($link, $query);
+
+    if ($result) {
+        mysqli_commit($link);
+        showAlertDialog("Επιτυχής εγγραφή");
+        return true;
+    } else {
+        mysqli_rollback($link);
+        showAlertDialog("Αδυναμία εισαγωγής του ξενοδοχείου στην ιστοσελίδα. Παρακαλώ προσπαθήστε αργότερα.");
+        return false;
+    }
+
+
+}
+
+?>
